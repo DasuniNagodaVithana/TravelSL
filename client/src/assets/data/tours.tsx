6f7ce23bcd7e44db4adf6b9f1240b0340938c872
DasuniNagodaVithana <130269542+DasuniNagodaVithana@users.noreply.github.com>
2024-08-19 10:24:51 +0530

    featured tours added

---
+++ client/src/assets/data/tours.tsx
@@ -0,0 +1,135 @@
+import kandyImg from "../images/kandy.jpg";
+import galleImg from "../images/galle (2).jpg";
+import sigiriyaImg from "../images/sigiriya.jpg";
+import idalgashinnaImg from "../images/idalgashinna.jpg";
+import bentotaImg from "../images/madhu-river-bentota-sri.jpg";
+import anuradhapuraImg from "../images/mirisaweti-stupa.jpg";
+
+interface Review {
+  name: string;
+  rating: number;
+}
+
+interface Tour {
+  id: string;
+  title: string;
+  city: string;
+  distance: number;
+  price: number;
+  maxGroupSize: number;
+  desc: string;
+  reviews: Review[];
+  avgRating: number;
+  photo: string;
+  featured: boolean;
+}
+
+const tours: Tour[] = [
+  {
+    id: "01",
+    title: "Temple of the Tooth",
+    city: "Kandy",
+    distance: 100,
+    price: 50,
+    maxGroupSize: 20,
+    desc: "Visit the sacred Temple of the Tooth in the heart of Kandy.",
+    reviews: [
+      {
+        name: "Sayumi Tharumila",
+        rating: 4.7,
+      },
+    ],
+    avgRating: 4.7,
+    photo: kandyImg,
+    featured: true,
+  },
+  {
+    id: "02",
+    title: "Galle Fort",
+    city: "Galle",
+    distance: 120,
+    price: 60,
+    maxGroupSize: 15,
+    desc: "Explore the historical Galle Fort, a UNESCO World Heritage Site.",
+    reviews: [
+      {
+        name: "Raveena Liyanage",
+        rating: 4.8,
+      },
+    ],
+    avgRating: 4.8,
+    photo: galleImg,
+    featured: true,
+  },
+  {
+    id: "03",
+    title: "Sigiriya Rock Fortress",
+    city: "Sigiriya",
+    distance: 150,
+    price: 75,
+    maxGroupSize: 10,
+    desc: "Climb the ancient Sigiriya Rock Fortress, an iconic landmark.",
+    reviews: [
+      {
+        name: "Alice Smith",
+        rating: 4.9,
+      },
+    ],
+    avgRating: 4.9,
+    photo: sigiriyaImg,
+    featured: true,
+  },
+  {
+    id: "04",
+    title: "Scenic Train Ride",
+    city: "Idalgashinna",
+    distance: 180,
+    price: 40,
+    maxGroupSize: 25,
+    desc: "Experience one of the most scenic train rides in the world.",
+    reviews: [
+      
+    ],
+    avgRating: 4.6,
+    photo: idalgashinnaImg,
+    featured: true,
+  },
+  {
+    id: "05",
+    title: "Bentota Beach",
+    city: "Bentota",
+    distance: 80,
+    price: 50,
+    maxGroupSize: 30,
+    desc: "Relax on the beautiful beaches of Bentota.",
+    reviews: [
+      {
+        name: "Nalina Udayana",
+        rating: 4.5,
+      },
+    ],
+    avgRating: 4.5,
+    photo: bentotaImg,
+    featured: true,
+  },
+  {
+    id: "06",
+    title: "Sacred City of Anuradhapura",
+    city: "Anuradhapura",
+    distance: 200,
+    price: 65,
+    maxGroupSize: 15,
+    desc: "Discover the ancient ruins of the Sacred City of Anuradhapura.",
+    reviews: [
+      {
+        name: "David Green",
+        rating: 4.7,
+      },
+    ],
+    avgRating: 4.7,
+    photo: anuradhapuraImg,
+    featured: true,
+  },
+];
+
+export default tours;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../styles/home.css'
 import { Container,Row,Col } from 'reactstrap'
+import FeaturedTourList from '../components/Featured-tours/FeaturedTourList'
 
 import sigiriya from "../assets/images/sigiriya.jpg"
 import ella from "../assets/images/ella (2).jpg"
@@ -67,6 +68,19 @@ const Home = () => {
       </Row>
     </Container>
   </section>
+  {/*==========featured tour section start======*/}
+  <section>
+    <Container>
+      <Row>
+        <Col lg='12' className='mb-5'>
+        <Subtitle Subtitle={'Explore'}/>
+        <h2 className='featured__tour-title'>Our featured tours</h2>
+        </Col>
+        <FeaturedTourList/>
+      </Row>
+    </Container>
+  </section>
+  {/*==========featured tour section end======*/}
   </>
 }
 
