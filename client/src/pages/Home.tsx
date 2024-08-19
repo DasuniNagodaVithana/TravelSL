ad37ac233c5d172b4466fa1658b977e5f69162ea
DasuniNagodaVithana <130269542+DasuniNagodaVithana@users.noreply.github.com>
2024-08-19 11:21:53 +0530

    experience section added

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,6 +8,7 @@ import ella from "../assets/images/ella (2).jpg"
 import videoSL from "../assets/images/videoSL.mp4"
 import Subtitle from '../shared/Subtitle'
 import worldImg from "../assets/images/world.png"
+import experirnce from "../assets/images/experience.png"
 
 import Searchbar from '../shared/Searchbar'
 import ServiceList from '../Services/ServiceList'
@@ -81,6 +82,46 @@ const Home = () => {
     </Container>
   </section>
   {/*==========featured tour section end======*/}
+
+  {/*==========experience section start======*/}
+   <section>
+    <Container>
+      <Row>
+        <Col lg='6'>
+        <div className="experience__content">
+          <Subtitle Subtitle={'Experience'}/>
+          <h2>
+            with our all experience <br/> we will serve you
+          </h2>
+          <p>Whether it's a luxurious getaway or an adventurous escape, we tailor each experience to your desires.<br/>
+            Trust us to craft unforgettable memories, curated just for you.
+          </p>
+        </div>
+
+        <div className="counter__wrapper d-flex align-items-center gap-5">
+        <div className="counter__box">
+          <span>12k+</span>
+          <h6>Sucessful Trips</h6>
+        </div>
+        <div className="counter__box">
+          <span>2k+</span>
+          <h6>Regular clients</h6>
+        </div>
+        <div className="counter__box">
+          <span>10</span>
+          <h6>Years of experience</h6>
+        </div>
+        </div>
+        </Col>
+        <Col lg='6'>
+        <div className="experience__img">
+          <img src={experirnce} alt=''/>
+        </div>
+        </Col>
+      </Row>
+    </Container>
+   </section>
+   {/*==========experience section end======*/}
   </>
 }
 
