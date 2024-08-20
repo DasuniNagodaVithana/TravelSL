05275a8a60fb6e3d1e6cb1550b7caf90f1a58bb5
DasuniNagodaVithana <130269542+DasuniNagodaVithana@users.noreply.github.com>
2024-08-20 08:55:05 +0530

    sticky header bar added

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,74 +1,71 @@
-import React from 'react'
-import { Container,Row,Button } from 'reactstrap'
-import { NavLink,Link } from 'react-router-dom'
-import logo from '../../assets/images/logo.png'
+import React, { useRef, useEffect } from 'react';
+import { Container, Row, Button } from 'reactstrap';
+import { NavLink, Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
 import 'remixicon/fonts/remixicon.css';
-import './header.css'
+import './header.css';
 
-const nav_links=[
+const nav_links = [
   {
-    path:'/home',
-    display:'Home'
+    path: '/home',
+    display: 'Home',
   },
-
   {
-    path:'/about',
-    display:'About'
+    path: '/about',
+    display: 'About',
   },
-
   {
-    path:'/tours',
-    display:'Tours'
+    path: '/tours',
+    display: 'Tours',
   },
+];
 
-]
+const Header = () => {
+  return (
+    <header className='header'>
+      <Container>
+        <Row>
+          <div className='nav__wrapper d-flex align-items-center justify-content-between'>
+            {/*=============logo=========*/}
+            <div className='logo'>
+              <img src={logo} alt='logo' />
+            </div>
+            {/*=============logo end=========*/}
 
-const header = () => {
-  return <header className='header'>
-    <Container>
-      <Row>
-        <div className='nav__wrapper d-flex align-items-center justify-content-between'>
-
-          {/*=============logo=========*/}
-          <div className='logo'>
-            <img src={logo} alt=''/>
-
-          </div>
-          {/*=============logo end=========*/}
-
-           {/*=============menu start=========*/}
-           <div className='navigation'>
-            <ul className='menu d-flex align-items-center gap-5'>
-              {nav_links.map((item,index)=>(
-                <li className='nav__item' key={index}>
-                  <NavLink to={item.path}
-                  className={navClass=>navClass.isActive?"active__link":""}
-                  >{item.display}</NavLink>
-
-                </li>
-              ))}
-
-            </ul>
-
-           </div>
+            {/*=============menu start=========*/}
+            <div className='navigation'>
+              <ul className='menu d-flex align-items-center gap-5'>
+                {nav_links.map((item, index) => (
+                  <li className='nav__item' key={index}>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) => (navClass.isActive ? 'active__link' : '')}
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
             {/*=============menu end=========*/}
-<div className='nav__right d-flex align-items-center gap-4'>
-  <div className='nav__btns d-flex align-items-center gap-4'>
-    <Button className='btn secondary__btn'><Link to='/login'>Login</Link>
-    </Button>
-    <Button className='btn primary__btn'><Link to='/register'>Register</Link>
-    </Button>
+            <div className='nav__right d-flex align-items-center gap-4'>
+              <div className='nav__btns d-flex align-items-center gap-4'>
+                <Button className='btn secondary__btn'>
+                  <Link to='/login'>Login</Link>
+                </Button>
+                <Button className='btn primary__btn'>
+                  <Link to='/register'>Register</Link>
+                </Button>
+              </div>
+              <span className='mobile__menu'>
+                <i className='ri-menu-fill'></i>
+              </span>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
+};
 
-  </div>
-  <span className="mobile__menu">
-  <i className="ri-menu-fill"></i>
-  </span>
-</div>
-
-        </div>
-      </Row>
-    </Container>
-  </header>
-}
-
-export default header
+export default Header;
